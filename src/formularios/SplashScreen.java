6ed881701fa3d990fa3f58aa4c66733c6ae2a96f
Samuel Ortiz <sortizm5@miumg.edu.gt>
2021-09-14 23:37:14 -0600

    Se incluye libreria para reproducir mp3 y se crea form para reproductor.

---
+++ src/formularios/SplashScreen.java
@@ -0,0 +1,151 @@
+package formularios;
+
+import java.awt.Image;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author Samuel David Ortiz
+ */
+public class SplashScreen extends javax.swing.JFrame implements Runnable {
+
+    private JLabel umg;
+    private ImageIcon Img;
+    private Icon icono;
+    private Thread tiempo = null;
+    
+    //FrmMusicPlayer player = new FrmMusicPlayer();
+
+    public SplashScreen() {
+        initComponents();
+        tiempo = new Thread(this);
+        tiempo.start();
+        paneles();
+    }
+
+    public void paneles() {
+
+        umg = new JLabel();
+        umg.setSize(100, 100);
+        umg.setLocation(0, 0);
+        umg.setText("Hola");
+
+        Img = new ImageIcon("./src/images/umg.png");
+        Img = new ImageIcon(Img.getImage().getScaledInstance(umg.getWidth(), umg.getHeight(),
+                Image.SCALE_DEFAULT));
+
+        umg.setIcon(Img);
+        umg.setVisible(true);
+        panelSplash.add(umg, 0);
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        panelSplash = new javax.swing.JPanel();
+        progressBar = new javax.swing.JProgressBar();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setUndecorated(true);
+
+        javax.swing.GroupLayout panelSplashLayout = new javax.swing.GroupLayout(panelSplash);
+        panelSplash.setLayout(panelSplashLayout);
+        panelSplashLayout.setHorizontalGroup(
+            panelSplashLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelSplashLayout.createSequentialGroup()
+                .addGap(27, 27, 27)
+                .addComponent(progressBar, javax.swing.GroupLayout.PREFERRED_SIZE, 531, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(42, Short.MAX_VALUE))
+        );
+        panelSplashLayout.setVerticalGroup(
+            panelSplashLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelSplashLayout.createSequentialGroup()
+                .addGap(134, 134, 134)
+                .addComponent(progressBar, javax.swing.GroupLayout.PREFERRED_SIZE, 24, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(142, Short.MAX_VALUE))
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(panelSplash, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(panelSplash, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        setSize(new java.awt.Dimension(600, 300));
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(SplashScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(SplashScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(SplashScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(SplashScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new SplashScreen().setVisible(true);
+            }
+        });
+    }
+    
+    @Override
+    public void run() {
+        try {
+            for (int i = 0; i<=100;i++){
+
+                Thread.sleep(5);
+
+                //progress.setText(Integer.toString(i)+"%");
+                progressBar.setValue(i);
+                /*clock hora = new clock();
+                Thread h1 = new Thread(hora.h1);
+                h1.start();
+                String hr = hora.hora();
+                reloj.setText(hr);*/
+            }
+            this.dispose();
+            new FrmMusicPlayer();
+        } catch (InterruptedException ex) {
+            Logger.getLogger(SplashScreen.class.getName()).log(Level.SEVERE, null, ex);
+        }
+            tiempo = null;
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel panelSplash;
+    private javax.swing.JProgressBar progressBar;
+    // End of variables declaration//GEN-END:variables
+}
