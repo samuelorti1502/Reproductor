786f91098d06c692a62020c2262ac4dab32ce8ad
Samuel Ortiz <sortizm5@miumg.edu.gt>
2021-09-16 23:15:46 -0600

    Implementacion de libreria para reproducir mp3

--- src/formularios/FrmMusicPlayer.java
+++ src/formularios/FrmMusicPlayer.java
@@ -1,56 +1,52 @@
 package formularios;
 
-import clases.FilterType;
-import clases.MusicTags;
-import com.mpatric.mp3agic.InvalidDataException;
-import com.mpatric.mp3agic.UnsupportedTagException;
 import jaco.mp3.player.MP3Player;
-import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.File;
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.ArrayList;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javax.swing.*;
-import jmusic.JMusicPlayerList;
-import jmusic.JMusicSong;
-import jmusic.MusicPlayerControl;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 /**
  *
  * @author Samuel David Ortiz
  */
-public class FrmMusicPlayer implements ActionListener{
+public class FrmMusicPlayer implements ActionListener {
 
     private JFrame player;
-    private JPanel panelInfo, panelPlayer;
+    private JPanel panelLista, panelInfo, panelPlayer;
     private ImageIcon Img;
     private Icon icono;
-    private JLabel play, stop, fwd, prev, ps, abrir;
-    //private JMenuBar menuBar;
+    private JLabel play, stop, fwd, prev, aleat, abrir;
     private JMenu menu, smenu;
     private JMenuItem e1, e2;
+    private JList lista;
 
     private boolean press = false;
 
-    File songFile;
+    File songFile[];
     String currentDirectory = "home.user";
     String currentPath;
     String imagePath;
-    
+
     private Media archivo;
     private MediaPlayer repro;
-    int seleccion, cont = 0, contAuxNext = 1;
+    private JFileChooser openFile;
+    int seleccion, cont = 0, contAuxNext = 1, indice = 0, indiceAuxNext, indiceAuxPrev;
+    private ArrayList arrayFormato = new ArrayList();
+    private ArrayList arrayNombreMusica = new ArrayList();
+    private ArrayList arrayRuta = new ArrayList();
+    String rutaFormato, rutaNombreMusica;
+    DefaultListModel modelo = new DefaultListModel();
 
     public FrmMusicPlayer() {
-        
-        
+
         Player();
         PlayPause();
     }
@@ -67,21 +63,21 @@ public class FrmMusicPlayer implements ActionListener{
         JMenuBar menuBar = new JMenuBar();
         menu = new JMenu("Archivo");
         e1 = new JMenuItem("Abrir");
-        e1.addActionListener(this); 
+        e1.addActionListener(this);
         menu.add(e1);
-        
+
         menuBar.add(menu);
         player.setJMenuBar(menuBar);
         // </editor-fold>
-        
+
         // <editor-fold defaultstate="collapsed" desc="Panel info">
-        panelInfo = new JPanel();
-        panelInfo.setSize(570, 50);
-        panelInfo.setLocation(5, 0);
-        panelInfo.setLayout(null);
-        panelInfo.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
-        panelInfo.setVisible(true);
-        player.add(panelInfo, 0);
+        panelLista = new JPanel();
+        panelLista.setSize(570, 150);
+        panelLista.setLocation(5, 0);
+        panelLista.setLayout(null);
+        panelLista.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        panelLista.setVisible(true);
+        player.add(panelLista, 0);
         // </editor-fold>
 
         // <editor-fold defaultstate="collapsed" desc="Panel Reproductor"> 
@@ -106,7 +102,7 @@ public class FrmMusicPlayer implements ActionListener{
         play.setIcon(icono);
         play.setVisible(true);
         // </editor-fold>
-        
+
         // <editor-fold defaultstate="collapsed" desc="Boton siguiente">
         fwd = new JLabel();
         fwd.setSize((panelPlayer.getWidth() / 10), 70);
@@ -130,65 +126,180 @@ public class FrmMusicPlayer implements ActionListener{
         prev.setIcon(icono);
         prev.setVisible(true);
         // </editor-fold>
-                
+        
+        // <editor-fold defaultstate="collapsed" desc="Boton aleatorio">
+        aleat = new JLabel();
+        aleat.setSize((panelPlayer.getWidth() / 10), 70);
+        aleat.setLocation(10, play.getY());
+        Img = new ImageIcon("./src/images/shuffle.png");
+        icono = new ImageIcon(Img.getImage().getScaledInstance(play.getWidth() - 15, play.getHeight() - 15,
+                Image.SCALE_DEFAULT));
+
+        aleat.setIcon(icono);
+        aleat.setVisible(true);
+        // </editor-fold>
+
+        // <editor-fold defaultstate="collapsed" desc="playlist">
+        lista = new JList();
+        lista.setLocation(10, 10);
+        lista.setSize(panelLista.getWidth() - 20, panelLista.getHeight() - 20);
+        lista.setVisible(true);
+        // </editor-fold>
+        
+        panelLista.add(lista, 0);
         panelPlayer.add(play, 0);
         panelPlayer.add(prev, 0);
         panelPlayer.add(fwd, 0);
+        panelPlayer.add(aleat, 0);
 
         player.setVisible(true);
     }
-    
+
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource() == e1){
-            //JOptionPane.showMessageDialog(null, "Hola");
-            JFileChooser openFile = new JFileChooser(currentDirectory);
-            openFile.setFileFilter(new FilterType(".mp3", "Abrir solo mp3"));
-            int result = openFile.showOpenDialog(null);
-            if(result == JFileChooser.APPROVE_OPTION){
-                songFile = openFile.getSelectedFile();
-                currentDirectory = songFile.getAbsolutePath();
-                try {
-                    MusicTags tags = new MusicTags(currentDirectory);
-                    System.out.println(tags.getArtist());
-                    System.out.println(tags.getTitle());
-                } catch (IOException ex) {
-                    Logger.getLogger(FrmMusicPlayer.class.getName()).log(Level.SEVERE, null, ex);
-                } catch (UnsupportedTagException ex) {
-                    Logger.getLogger(FrmMusicPlayer.class.getName()).log(Level.SEVERE, null, ex);
-                } catch (InvalidDataException ex) {
-                    Logger.getLogger(FrmMusicPlayer.class.getName()).log(Level.SEVERE, null, ex);
-                }
-                
-                System.out.println(currentDirectory);
-                
-            }
+        if (e.getSource() == e1) {
+            SeleccionarMusica();
         }
     }
-    
+
+    public void SeleccionarMusica() {
+        openFile = new JFileChooser();
+        openFile.setFileFilter(new FileNameExtensionFilter("Archivos *MP3", "mp3"));
+        openFile.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        openFile.setMultiSelectionEnabled(true);
+        int selec = openFile.showOpenDialog(null);
+
+        if (selec == JFileChooser.APPROVE_OPTION) {
+            File files[] = openFile.getSelectedFiles();
+
+            for (File file : files) {
+                arrayNombreMusica.add(file.getName());
+                arrayRuta.add(file.getPath());
+                //DefaultListModel modelo = (DefaultListModel) lista.getModel();
+                modelo.addElement(arrayNombreMusica.get(cont));
+                lista.setModel(modelo);
+                arrayFormato.add(file.toURI().toString());
+                System.out.println(arrayFormato.get(cont));
+                System.out.println(arrayRuta.get(cont));
+                cont++;
+            }
+        } else {
+            JOptionPane.showMessageDialog(null, "No selecciono ningun archivo");
+        }
+
+    }
 
     public void PlayPause() {
         play.addMouseListener(new MouseAdapter() {
             public void mousePressed(MouseEvent e) {
-                if (!isPress()) {
-                    Img = new ImageIcon("./src/images/pause.png");
-                    setPress(true);
+                if (arrayFormato.size() == 0) {
+                    JOptionPane.showMessageDialog(null, "No hay musica para reproducir");
                 } else {
-                    Img = new ImageIcon("./src/images/play.png");
-                    setPress(false);
+                    if (!isPress()) {
+                        Img = new ImageIcon("./src/images/pause.png");
+                        indice = lista.getSelectedIndex();
+                        if (repro != null) {
+                            repro.play();
+                        } else {
+                            play(indice);
+                        }
+                        setPress(true);
+                    } else {
+                        Img = new ImageIcon("./src/images/play.png");
+                        if (repro != null) {
+                            repro.pause();
+                        }
+                        setPress(false);
+                    }
+
+                    icono = new ImageIcon(Img.getImage().getScaledInstance(play.getWidth(), play.getHeight(),
+                            Image.SCALE_DEFAULT));
+
+                    play.setIcon(icono);
                 }
 
-                icono = new ImageIcon(Img.getImage().getScaledInstance(play.getWidth(), play.getHeight(),
-                        Image.SCALE_DEFAULT));
-
-                play.setIcon(icono);
             }
         });
-        
+
         fwd.addMouseListener(new MouseAdapter() {
             public void mousePressed(MouseEvent e) {
-                System.out.println("holaaaa");
+                if (arrayFormato.size() == 0) {
+                    JOptionPane.showMessageDialog(null, "No hay musica para reproducir");
+                } else {
+                    if (repro == null) {
+                        JOptionPane.showMessageDialog(null, "No hay musica reproduciondose");
+                    } else {
+                        repro.dispose();
+                        indiceAuxNext = indice + contAuxNext;
+
+                        if (indiceAuxNext < arrayFormato.size()) {
+                            next(indiceAuxNext);
+                            contAuxNext++;
+                        } else {
+                            contAuxNext = 0;
+                        }
+                    }
+
+                }
             }
         });
+
+        prev.addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent e) {
+                if (arrayFormato.size() == 0) {
+                    JOptionPane.showMessageDialog(null, "No hay musica para reproducir");
+                } else {
+                    if (repro == null) {
+                        JOptionPane.showMessageDialog(null, "No hay musica reproduciondose");
+                    } else {
+                        indice--;
+                        repro.dispose();
+                        indiceAuxPrev = indice;
+
+                        if (indiceAuxPrev >= 0) {
+                            prev(indiceAuxPrev);
+
+                        } else {
+                            indice = arrayFormato.size() - 1;
+                        }
+                    }
+
+                }
+            }
+        });
+    }
+
+    public void play(int indice) {
+        com.sun.javafx.application.PlatformImpl.startup(() -> {
+        });
+
+        rutaFormato = (String) arrayFormato.get(indice).toString();
+        rutaNombreMusica = (String) arrayNombreMusica.get(indice).toString();
+
+        archivo = new Media(rutaFormato);
+        repro = new MediaPlayer(archivo);
+        repro.play();
+    }
+
+    public void next(int indiceAux) {
+        com.sun.javafx.application.PlatformImpl.startup(() -> {
+        });
+
+        rutaFormato = (String) arrayFormato.get(indiceAux).toString();
+        rutaNombreMusica = (String) arrayNombreMusica.get(indiceAux).toString();
+        archivo = new Media(rutaFormato);
+        repro = new MediaPlayer(archivo);
+        repro.play();
+    }
+
+    public void prev(int indiceAux) {
+        com.sun.javafx.application.PlatformImpl.startup(() -> {
+        });
+
+        rutaFormato = (String) arrayFormato.get(indiceAux).toString();
+        rutaNombreMusica = (String) arrayNombreMusica.get(indiceAux).toString();
+        archivo = new Media(rutaFormato);
+        repro = new MediaPlayer(archivo);
+        repro.play();
     }
 
     public boolean isPress() {
